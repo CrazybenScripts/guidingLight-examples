98a9ab9fbcbc18835f1c2951bd6aaa51d0f52483
CrazybenScripts <144243491+CrazybenScripts@users.noreply.github.com>
2024-09-10 16:48:14 -0600

    Update kill-brick.lua

--- kill-brick.lua
+++ kill-brick.lua
@@ -1,6 +1,7 @@
 local killBrick = script.Parent -- replace with location of part
 
 killBrick.Touched:Connect(function(hit)
+    if not hit.Parent:FindFirstChild("Humanoid") then return end
     local plr = game.Players:GetPlayerFromCharacter(hit.Parent)
     plr.Character.Humanoid:TakeDamage(100)
     game.ReplicatedStorage.Death:FireClient(plr, {
