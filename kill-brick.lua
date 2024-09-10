eedb0996e83720bccc3a500eee881d7a77e8f9b7
CrazybenScripts <144243491+CrazybenScripts@users.noreply.github.com>
2024-09-10 16:46:21 -0600

    Update kill-brick.lua

--- kill-brick.lua
+++ kill-brick.lua
@@ -2,6 +2,7 @@ local killBrick = script.Parent -- replace with location of part
 
 killBrick.Touched:Connect(function(hit)
     local plr = game.Players:GetPlayerFromCharacter(hit.Parent)
+    plr.Charcter.Humanoid:TakeDamage(100)
     game.ReplicatedStorage.Death:FireClient(plr, {
       -- Messages for guiding light would be here --
       --- Example: "You died to Rush"; --
